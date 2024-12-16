6a9120bad45b3c4a525864774bf547b7a4f84dc7
enriquesnts <ayo4175@gmail.com>
2024-12-15 22:11:14 -0800

    inicio de proyecto

---
+++ movie-review-backend/src/main/java/uv/mx/movie_review_backend/MovieReviewBackendApplication.java
@@ -0,0 +1,13 @@
+package uv.mx.movie_review_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewBackendApplication.class, args);
+	}
+
+}
